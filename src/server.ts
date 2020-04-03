39788a71498a746c4755b06fd52388bd6ea2eb25
FDSoftware <dfrenoux@gmail.com>
2020-04-03 16:08:23 -0300

    fxi cors header

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
   // authorized headers for preflight requests
   // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,client_secret');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-auth-token');
   next();
 });
 
